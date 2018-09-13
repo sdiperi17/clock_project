128630792c8a2b8ecb9cacd39ba502456846e716
sdiperi90 <sirgasheva17@gmail.com>
2018-09-12 22:48:10 -0400

    created variables for the position of clockhands

--- script.js
+++ script.js
@@ -2,3 +2,6 @@ const HOURHAND = document.querySelector("#hour");
 const MINUTEHAND = document.querySelector("#minute");
 const SECONDHAND = document.querySelector("#second");
 
+let hrPosition = 90;
+let minPosition = 120;
+let secPosition = 240;
